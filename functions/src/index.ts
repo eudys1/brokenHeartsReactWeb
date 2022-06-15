ba58664e136b52733139c65a1bc9d98deab88a52
eudys1 <eudysrosario01@gmail.com>
2022-06-16 00:31:10 +0200

    trying to deploid2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,3 @@
-// import * as functions from "firebase-functions";
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 // const functions = require('firebase-functions');
