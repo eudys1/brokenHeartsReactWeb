bee66e34659352bd71d71190131b6ce9b9395ad7
eudys1 <eudysrosario01@gmail.com>
2022-06-16 00:29:27 +0200

    trying to deploid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 // const functions = require('firebase-functions');
@@ -7,20 +7,20 @@ import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.deleteUser = functions.https.onCall(async (data, context) => {
-	// if (!context.auth.token.admin) return
-	const { uid } = data;
-	if (!uid) return { error: "Please enter an UID" };
-	try {
-		// return the promise from here
-		// await 
-		const user = await admin.auth();
+// exports.deleteUser = functions.https.onCall(async (data, context) => {
+// 	// if (!context.auth.token.admin) return
+// 	const { uid } = data;
+// 	if (!uid) return { error: "Please enter an UID" };
+// 	try {
+// 		// return the promise from here
+// 		// await 
+// 		const user = await admin.auth();
 
-		// console.log("Successfully deleted user");
-		return console.log(user);
+// 		// console.log("Successfully deleted user");
+// 		return console.log(user);
 
-	} catch (error) {
-		console.log("error deleting user", error);
-		return { error };
-	}
-});
+// 	} catch (error) {
+// 		console.log("error deleting user", error);
+// 		return { error };
+// 	}
+// });
