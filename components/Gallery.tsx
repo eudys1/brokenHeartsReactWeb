7ddc66db010b20186726e5f473d06eea09175874
eudys1 <eudysrosario01@gmail.com>
2022-06-17 00:53:29 +0200

    trying deploid2

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -115,9 +115,9 @@ export default function Gallery() {
 
             {isLoading ?
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38" stroke="#2286FF" className='h-24 w-h-24 mx-auto lg:mt-28'>
-                    <g fill="none" fill-rule="evenodd">
-                        <g transform="translate(1 1)" stroke-width="2">
-                            <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+                    <g fill="none" fillRule="evenodd">
+                        <g transform="translate(1 1)" strokeWidth="2">
+                            <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
                             <path d="M36 18c0-9.94-8.06-18-18-18">
                                 <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite" />
                             </path>
